9b89856231838b3431255b7a4690642460959230
varatharaj24 <144923343+varatharajH@users.noreply.github.com>
2024-03-10 17:15:36 +0530

    first commit

---
+++ post-method/src/main/java/com/postmethod/web/PostMethodApplication.java
@@ -0,0 +1,13 @@
+package com.postmethod.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMethodApplication.class, args);
+	}
+
+}
